0e28d47d7168585d17cd5d3f3a1eef24dcb5eb07
rakhawardhana <rakhagustiwardhana@gmail.com>
2020-04-10 15:09:49 +0700

    feat(jojonomic): implement .env configuration

--- src/connection/index.js
+++ src/connection/index.js
@@ -2,11 +2,11 @@ const mysql = require('mysql')
 
 const conn = mysql.createConnection(
     {
-        user: 'root',
-        password: 'Mysql123',
-        host: 'localhost',
-        database: 'jojonomic_test',
-        port : 3306
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        port : process.env.DB_PORT
     }
 )
 
